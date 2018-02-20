a4abf4e3eeb5a9305be24028d1d56c8795e7c0ab
46378 <YanL1393@sduhsd.lan>
2018-02-20 09:34:50 -0800

    Unit 6

--- Unit8/src/Lab14d.java
+++ Unit8/src/Lab14d.java
@@ -9,7 +9,13 @@ public class Lab14d
 	public static void main( String args[] )
 	{
 		//add test cases
-		
-		
+		Grades monroe = new Grades(new double[]{100,90,85,72.5,95.6});
+		System.out.println(monroe);
+		monroe.setGrades(new double[]{50,100,80});
+		System.out.println(monroe);
+		monroe.setGrades(new double[]{93,-90,90});
+		System.out.println(monroe);
+		monroe.setGrades(new double[]{1,2,3,4,5,6,7,8,9});
+		System.out.println(monroe);
 	}
 }
